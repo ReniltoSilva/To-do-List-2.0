2a294db3fb2228c781182e825a729d748e3e5ada
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2024-07-25 00:08:03 -0500

    updates

--- todo3-0.js
+++ todo3-0.js
@@ -130,8 +130,7 @@ const task = new Task('1', 'Comprar carne', true, false, 'low')
                        
                     
                     
-    //             mainContainer.appendChild(taskContainer)
-    //         }
+
 
 
 
