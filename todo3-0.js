79d886c796b35daf326ac911100faa2c3de010d8
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2025-02-10 08:31:35 -0500

    updates

--- todo3-0.js
+++ todo3-0.js
@@ -3,10 +3,12 @@ const form = document.querySelector('.formContainer')
 const input = document.querySelector('.inputContainer')
 const ulContainer = document.querySelector('.generalTaskContainer')
 
+document.addEventListener('DOMContentLoaded', loadFromLS())
+
 
 const arrayList = []
 
-
+//Add Tasks
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -15,7 +17,45 @@ form.addEventListener('submit', (e) => {
         id: (Math.random()*1000*1000).toFixed(0)
     })
 
+    saveLS()
+
     input.value = ''
 
     console.log(arrayList)
-})
+}) 
+
+
+//Save to LocalStorage
+function saveLS(){
+    localStorage.setItem('tasks', JSON.stringify(arrayList))
+}
+
+
+//Load from LocalStorage
+function loadFromLS(){
+    const savedArray = JSON.parse(localStorage.getItem('tasks'))
+
+    savedArray.forEach((item) => {
+        const liElement = document.createElement('li')
+        liElement.classList.add('taskContainer')
+        liElement.innerHTML = `<span class="textContainerJS">
+                                ${item.task}
+                                </span>
+                                <div class="delBtnJsContainer">
+                                    <button class="delBtnJS">Delete</button>
+                                </div>`
+
+        liElement.addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+            liElement.querySelector('.textContainerJS').classList.toggle('strikeThrough')
+        })
+
+        liElement.querySelector('.delBtnJS').addEventListener('click', () => {
+            liElement.remove()
+        })
+
+        ulContainer.appendChild(liElement)
+    })
+
+    console.log(savedArray)
+}
