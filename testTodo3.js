7ca04b217fc36d3953a5d9e9247a45d87089b3a8
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2025-02-26 00:43:12 -0500

    Appended tasks to the DOM

--- testTodo3.js
+++ testTodo3.js
@@ -57,30 +57,37 @@ function renderLists(arrayOfLists){
             const formInputContainer = document.createElement('form')
                 const inputContainer = document.createElement('input')
         const ulContainer = document.createElement('ul')
+            // const liJS = document.createElement('li')
+            //         const spanJS = document.createElement('span')
+            //         const delBtnContainer = document.createElement('div');
+            //             const delBtn = document.createElement('delBtnJS')
+
 
         //ADD CLASSES
         mainContainer.classList.add('mainContainer')
             titleListContainer.classList.add('titleListContainer')
                 titleListContainer.setAttribute('contenteditable', 'true')
                 titleListContainer.textContent = `${list.listTitle}`
-
             formGeneralContainer.classList.add('formGeneralContainer')
                 formInputContainer.classList.add('formContainer')
+
+                //ADD AND SAVE TASKS FROM EACH LIST TO LS
                 formInputContainer.addEventListener('submit', (e) => {
                     e.preventDefault()
 
-                    console.log(arrayOfLists[index].tasks)
-
-                    arrayOfLists[index].tasks.push({
+                    if(inputContainer.value === ''){
+                        alert('Please, add a task')
+                    }else{
+                      arrayOfLists[index].tasks.push({
                         taskID: (Math.random()*1000*1000).toFixed(0),
                         taskTitle: inputContainer.value,
-                        taskDone: false
+                        taskDone: false  
                     })
-
-                    console.log(arrayOfLists[index].tasks)
-                    console.log(arrayOfLists)
+                    }
 
                     localStorage.setItem('Main Array Lists', JSON.stringify(arrayOfLists))
+
+                    renderLists(arrayOfLists)
 
                     inputContainer.value = ''
                 })
@@ -88,6 +95,34 @@ function renderLists(arrayOfLists){
                     inputContainer.setAttribute('placeholder', 'Add a task')
 
                 ulContainer.classList.add('generalTaskContainer')
+                    // liJS.classList.add('taskContainer')
+                    //             spanJS.classList.add('textContainerJS')
+                    //             spanJS.textContent = arrayOfLists[index].tasks.taskTitle  
+                    //             delBtnContainer.classList.add('delBtnJsContainer');
+
+                console.log(arrayOfLists[index].tasks)
+                arrayOfLists[index].tasks.forEach((task) => {
+
+                    //Create task elements
+                    const liJS = document.createElement('li')
+                    const spanJS = document.createElement('span')
+                    const delBtnContainer = document.createElement('div');
+                        const delBtn = document.createElement('delBtnJS')
+
+                    //Add classes to task elements
+                    liJS.classList.add('taskContainer')
+                    spanJS.classList.add('textContainerJS')
+                    spanJS.textContent = task.taskTitle  
+                    delBtnContainer.classList.add('delBtnJsContainer');
+
+                    //Append task elements
+                    ulContainer.appendChild(liJS)
+                    liJS.append(spanJS, delBtnContainer)
+                    delBtnContainer.appendChild(delBtn)
+
+                })
+
+
 
 
         //APPEND ELEMENTS       
@@ -95,5 +130,14 @@ function renderLists(arrayOfLists){
         mainContainer.append(titleListContainer, formGeneralContainer, ulContainer)
             formGeneralContainer.appendChild(formInputContainer)
                 formInputContainer.appendChild(inputContainer)
+                // ulContainer.appendChild(liJS)
+                //     liJS.append(spanJS, delBtnContainer)
+                //     delBtnContainer.appendChild(delBtn)
     })
 }
+
+
+// //RENDER LIST TASKS FROM LS
+// function renderTasks(){
+
+// }
