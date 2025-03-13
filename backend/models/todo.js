61918a43c168e187bad897c8e4df8532c593e1fe
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2025-03-13 05:46:36 -0500

    updates

--- backend/models/todo.js
+++ backend/models/todo.js
@@ -1,29 +1,14 @@
 import mongoose from "mongoose";
 
-// const TodoSchema = new mongoose.Schema({
-//     title: { type: String, required: true },
-//     completed: { type: Boolean, default: false }
-// });
-
-// const TaskSchema = new mongoose.Schema({
-//     taskID: { type: String, required: true },
-//     taskTitle: { type: String, required: true },
-//     taskDone: { type: Boolean, default: false }
-// });
-
-// const TodoSchema = new mongoose.Schema({
-//     listTitle: { type: String, required: true },
-//     tasks: { type: [TaskSchema], default: [] } // ✅ Allows an empty array or an array of tasks
-// });
 
 const TaskSchema = new mongoose.Schema({
-    taskID: String,
-    taskTitle: String,
-    taskDone: Boolean
+    taskID: { type: String, required: true },
+    taskTitle: { type: String, required: true },
+    taskDone: { type: Boolean, default: false }
 });
 
 const TodoSchema = new mongoose.Schema({
-    listTitle: String,
+    listTitle: { type: String, required: true },
     tasks: { type: [TaskSchema], default: [] } // ✅ Allows an empty array or an array of tasks
 });
 
